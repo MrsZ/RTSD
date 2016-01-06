f62a8ec127aefa566382136510a3ece6a08affb8
Olli <olkorhon@student.oulu.fi>
2016-01-06 15:04:45 +0200

    Unified fonts to support multiplatform approach.

--- RTSD_form/RTSD_form/Form1.Designer.cs
+++ RTSD_form/RTSD_form/Form1.Designer.cs
@@ -62,11 +62,11 @@
             this.tabControl.Controls.Add(this.tabPage_chats);
             this.tabControl.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl.ItemSize = new System.Drawing.Size(120, 40);
-            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Location = new System.Drawing.Point(14, 14);
             this.tabControl.Multiline = true;
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(474, 480);
+            this.tabControl.Size = new System.Drawing.Size(553, 554);
             this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControl.TabIndex = 1;
             // 
@@ -79,7 +79,7 @@
             this.tabPage_calls.Location = new System.Drawing.Point(4, 44);
             this.tabPage_calls.Name = "tabPage_calls";
             this.tabPage_calls.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_calls.Size = new System.Drawing.Size(466, 432);
+            this.tabPage_calls.Size = new System.Drawing.Size(545, 506);
             this.tabPage_calls.TabIndex = 2;
             this.tabPage_calls.Text = "Call";
             this.tabPage_calls.UseVisualStyleBackColor = true;
@@ -88,19 +88,19 @@
             // 
             this.groupBox1.Controls.Add(this.label_status);
             this.groupBox1.Controls.Add(this.label_status_message);
-            this.groupBox1.Location = new System.Drawing.Point(15, 102);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(12);
+            this.groupBox1.Location = new System.Drawing.Point(17, 118);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(14, 14, 14, 14);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(436, 315);
+            this.groupBox1.Size = new System.Drawing.Size(509, 363);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             // 
             // label_status
             // 
-            this.label_status.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_status.Location = new System.Drawing.Point(6, 19);
+            this.label_status.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_status.Location = new System.Drawing.Point(7, 22);
             this.label_status.Name = "label_status";
-            this.label_status.Size = new System.Drawing.Size(424, 43);
+            this.label_status.Size = new System.Drawing.Size(495, 50);
             this.label_status.TabIndex = 3;
             this.label_status.Text = "Current Status";
             this.label_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -108,11 +108,11 @@
             // label_status_message
             // 
             this.label_status_message.BackColor = System.Drawing.Color.Transparent;
-            this.label_status_message.Font = new System.Drawing.Font("Lucida Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_status_message.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_status_message.ForeColor = System.Drawing.Color.OliveDrab;
-            this.label_status_message.Location = new System.Drawing.Point(6, 62);
+            this.label_status_message.Location = new System.Drawing.Point(7, 72);
             this.label_status_message.Name = "label_status_message";
-            this.label_status_message.Size = new System.Drawing.Size(424, 250);
+            this.label_status_message.Size = new System.Drawing.Size(495, 288);
             this.label_status_message.TabIndex = 4;
             this.label_status_message.Text = "Trolling";
             this.label_status_message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -120,27 +120,30 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(9, 56);
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(10, 63);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 31);
+            this.button1.Size = new System.Drawing.Size(131, 36);
             this.button1.TabIndex = 2;
             this.button1.Text = "Call";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(9, 27);
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(10, 31);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(236, 23);
+            this.textBox1.Size = new System.Drawing.Size(275, 22);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 6);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 7);
             this.label1.Margin = new System.Windows.Forms.Padding(3);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 15);
+            this.label1.Size = new System.Drawing.Size(135, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "TARGET SIP ADDRESS";
             // 
@@ -152,7 +155,7 @@
             this.tabPage_chats.Controls.Add(this.label3);
             this.tabPage_chats.Location = new System.Drawing.Point(4, 44);
             this.tabPage_chats.Name = "tabPage_chats";
-            this.tabPage_chats.Size = new System.Drawing.Size(466, 432);
+            this.tabPage_chats.Size = new System.Drawing.Size(545, 506);
             this.tabPage_chats.TabIndex = 3;
             this.tabPage_chats.Text = "Chat";
             this.tabPage_chats.UseVisualStyleBackColor = true;
@@ -164,72 +167,78 @@
             this.groupBox2.Controls.Add(this.richTextBox2);
             this.groupBox2.Controls.Add(this.richTextBox1);
             this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Location = new System.Drawing.Point(12, 102);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(12);
+            this.groupBox2.Location = new System.Drawing.Point(14, 118);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(14, 14, 14, 14);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(442, 318);
+            this.groupBox2.Size = new System.Drawing.Size(516, 367);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(291, 255);
-            this.button3.Margin = new System.Windows.Forms.Padding(10);
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(339, 294);
+            this.button3.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(138, 50);
+            this.button3.Size = new System.Drawing.Size(161, 58);
             this.button3.TabIndex = 7;
             this.button3.Text = "Send";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(6, 248);
+            this.richTextBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(7, 286);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(272, 64);
+            this.richTextBox2.Size = new System.Drawing.Size(317, 73);
             this.richTextBox2.TabIndex = 5;
             this.richTextBox2.Text = "";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(6, 65);
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(7, 75);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(430, 177);
+            this.richTextBox1.Size = new System.Drawing.Size(501, 204);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 19);
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 22);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(424, 43);
+            this.label4.Size = new System.Drawing.Size(495, 50);
             this.label4.TabIndex = 3;
             this.label4.Text = "Chat Room";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(9, 56);
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(10, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(60, 31);
+            this.button2.Size = new System.Drawing.Size(70, 36);
             this.button2.TabIndex = 5;
             this.button2.Text = "Chat";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(9, 27);
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(10, 31);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(236, 23);
+            this.textBox2.Size = new System.Drawing.Size(275, 22);
             this.textBox2.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 6);
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 7);
             this.label3.Margin = new System.Windows.Forms.Padding(3);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(142, 15);
+            this.label3.Size = new System.Drawing.Size(135, 15);
             this.label3.TabIndex = 3;
             this.label3.Text = "TARGET SIP ADDRESS";
             // 
@@ -237,37 +246,39 @@
             // 
             this.label_login.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.label_login.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_login.Location = new System.Drawing.Point(399, 15);
+            this.label_login.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_login.Location = new System.Drawing.Point(465, 17);
             this.label_login.Name = "label_login";
-            this.label_login.Size = new System.Drawing.Size(247, 16);
+            this.label_login.Size = new System.Drawing.Size(288, 24);
             this.label_login.TabIndex = 3;
             this.label_login.Text = "Not logged in";
             this.label_login.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // richTextBox_log
             // 
-            this.richTextBox_log.Location = new System.Drawing.Point(492, 72);
+            this.richTextBox_log.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox_log.Location = new System.Drawing.Point(574, 83);
             this.richTextBox_log.Name = "richTextBox_log";
-            this.richTextBox_log.Size = new System.Drawing.Size(240, 420);
+            this.richTextBox_log.Size = new System.Drawing.Size(279, 484);
             this.richTextBox_log.TabIndex = 3;
             this.richTextBox_log.Text = "";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(492, 56);
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(574, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(240, 13);
+            this.label2.Size = new System.Drawing.Size(280, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "Event Log";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button_login_logout
             // 
-            this.button_login_logout.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_login_logout.Location = new System.Drawing.Point(652, 12);
+            this.button_login_logout.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_login_logout.Location = new System.Drawing.Point(761, 14);
             this.button_login_logout.Name = "button_login_logout";
-            this.button_login_logout.Size = new System.Drawing.Size(80, 24);
+            this.button_login_logout.Size = new System.Drawing.Size(93, 28);
             this.button_login_logout.TabIndex = 5;
             this.button_login_logout.Text = "Login";
             this.button_login_logout.UseVisualStyleBackColor = true;
@@ -275,15 +286,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Pink;
-            this.ClientSize = new System.Drawing.Size(744, 504);
+            this.ClientSize = new System.Drawing.Size(868, 582);
             this.Controls.Add(this.button_login_logout);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.richTextBox_log);
             this.Controls.Add(this.label_login);
             this.Controls.Add(this.tabControl);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl.ResumeLayout(false);
