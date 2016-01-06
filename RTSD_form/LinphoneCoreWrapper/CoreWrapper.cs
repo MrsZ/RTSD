6d5c3f9f56f8d00782f78938b35424b2a06fb4c9
Olli <olkorhon@student.oulu.fi>
2016-01-06 16:58:56 +0200

    Added phone as a new dll.

--- RTSD_form/LinphoneCoreWrapper/CoreWrapper.cs
+++ RTSD_form/LinphoneCoreWrapper/CoreWrapper.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace LinphoneCoreWrapper
 {
     public class CoreWrapper
     {
+        private const string LIBRARY_NAME = "Linphone.dll";
 
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void linphone_core_destruction();
     }
 }
